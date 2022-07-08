e9d3645cbd59801b92b91f25144d255289415185
13 <katie87827@gmail.com>
2022-07-08 19:24:02 +0800

    feat: add knowledge map.

--- View/Map/index.js
+++ View/Map/index.js
@@ -87,141 +87,142 @@ $(document).ready(function () {
             }
         },
     });
+    //
 
-    const topicData = [
-        {
-            id: '1',
-            title: '人工智慧簡介',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-01.jpg',
-            openStatus: true,
-        },
-        {
-            id: '2',
-            title: '資料收集與前處理',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-02.jpg',
-            openStatus: true,
-        },
-        {
-            id: '3',
-            title: '特徵的選擇',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-03.jpg',
-            openStatus: false,
-        },
-        {
-            id: '4',
-            title: '特徵的標準化',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-04.jpg',
-            openStatus: false,
-        },
-        {
-            id: '5',
-            title: '數劇集分割',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-01.jpg',
-            openStatus: false,
-        },
-        {
-            id: '6',
-            title: '監督式學習',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-02.jpg',
-            openStatus: false,
-        },
-        {
-            id: '7',
-            title: '最短距離、KNN',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-03.jpg',
-            openStatus: false,
-        },
-        {
-            id: '8',
-            title: '決策樹的原理',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-04.jpg',
-            openStatus: false,
-        },
-        {
-            id: '9',
-            title: '決策樹實作',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-01.jpg',
-            openStatus: false,
-        },
-        {
-            id: '10',
-            title: 'K-means 分群',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-02.jpg',
-            openStatus: false,
-        },
-        {
-            id: '11',
-            title: '非監督式學習',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-03.jpg',
-            openStatus: false,
-        },
-        {
-            id: '12',
-            title: '階層式分群',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            bgImage: '../../Images/map-04.jpg',
-            openStatus: false,
-        },
-    ];
+    // const topicData = [
+    //     {
+    //         id: '1',
+    //         title: '人工智慧簡介',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-01.jpg',
+    //         openStatus: true,
+    //     },
+    //     {
+    //         id: '2',
+    //         title: '資料收集與前處理',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-02.jpg',
+    //         openStatus: true,
+    //     },
+    //     {
+    //         id: '3',
+    //         title: '特徵的選擇',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-03.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '4',
+    //         title: '特徵的標準化',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-04.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '5',
+    //         title: '數劇集分割',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-01.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '6',
+    //         title: '監督式學習',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-02.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '7',
+    //         title: '最短距離、KNN',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-03.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '8',
+    //         title: '決策樹的原理',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-04.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '9',
+    //         title: '決策樹實作',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-01.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '10',
+    //         title: 'K-means 分群',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-02.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '11',
+    //         title: '非監督式學習',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-03.jpg',
+    //         openStatus: false,
+    //     },
+    //     {
+    //         id: '12',
+    //         title: '階層式分群',
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    //         bgImage: '../../Images/map-04.jpg',
+    //         openStatus: false,
+    //     },
+    // ];
 
-    const topicTemplate = `
-    <div class="card-wrap {{disable}}" topic-id="{{id}}">
-        <div class="card">
-            <div
-                class="card-bg"
-                style="background-image: url('../../Images/map-0{{imgIndex}}.jpg')"
-            ></div>
-            <div class="card-info">
-                <h3>{{title}}</h3>
-                <p>{{description}}</p>
-            </div>
-        </div>
-    </div>
-    `;
+    // const topicTemplate = `
+    // <div class="card-wrap {{disable}}" topic-id="{{id}}">
+    //     <div class="card">
+    //         <div
+    //             class="card-bg"
+    //             style="background-image: url('../../Images/map-0{{imgIndex}}.jpg')"
+    //         ></div>
+    //         <div class="card-info">
+    //             <h3>{{title}}</h3>
+    //             <p>{{description}}</p>
+    //         </div>
+    //     </div>
+    // </div>
+    // `;
 
-    topicData.forEach((item, index) => {
-        $('#mapContent').append(
-            generateTopicCard({
-                id: item.id,
-                index: index,
-                openStatus: item.openStatus,
-                title: item.title,
-                description: item.description,
-            }),
-        );
-    });
+    // topicData.forEach((item, index) => {
+    //     $('#mapContent').append(
+    //         generateTopicCard({
+    //             id: item.id,
+    //             index: index,
+    //             openStatus: item.openStatus,
+    //             title: item.title,
+    //             description: item.description,
+    //         }),
+    //     );
+    // });
 
-    function generateTopicCard(props) {
-        let template = topicTemplate;
+    // function generateTopicCard(props) {
+    //     let template = topicTemplate;
 
-        return generateHtml(template, {
-            imgIndex: (props.index % 4) + 1,
-            disable: props.openStatus ? '' : 'disable',
-            ...props,
-        });
-    }
+    //     return generateHtml(template, {
+    //         imgIndex: (props.index % 4) + 1,
+    //         disable: props.openStatus ? '' : 'disable',
+    //         ...props,
+    //     });
+    // }
 
-    $('#mapContent').slick({
-        infinite: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        arrows: false,
-    });
+    // $('#mapContent').slick({
+    //     infinite: false,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 4,
+    //     arrows: false,
+    // });
 
-    $(window).resize(function () {
-        $('#mapContent').slick('resize');
-    });
+    // $(window).resize(function () {
+    //     $('#mapContent').slick('resize');
+    // });
 
     $('.card-wrap:not(".disable")').on('click', function () {
         const topicID = $(this).attr('topic-id');
