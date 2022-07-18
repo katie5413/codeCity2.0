7f1d0dcf8004150890b1c4c7758f1a20b915358c
13 <katie87827@gmail.com>
2022-07-18 22:42:14 +0800

    fix: Correct addLesson database name.

--- API/addLesson.php
+++ API/addLesson.php
@@ -12,7 +12,7 @@ if (isset($_POST['name']) && isset($_POST['topic_ID'])) {
     $contentOrder = $_POST['contentOrder'];
 
 
-    $addLesson = $dbh->prepare('INSERT INTO lesson (topic_ID, title, introduction,contentOrder ) VALUES (?, ?, ?, ?)');
+    $addLesson = $dbh->prepare('INSERT INTO lesson (topic_ID, title, introduction,content_order ) VALUES (?, ?, ?, ?)');
     $addLesson->execute(array($topicID, $name, $introduction, $contentOrder));
 
     $lessonData = array("name" => $name, "introduction" => $introduction);
--- API/addLessonContent.php
+++ API/addLessonContent.php
@@ -12,7 +12,7 @@ if (isset($_POST['lesson_ID']) && isset($_POST['contentType']) && isset($_POST['
     $contentOrder = $_POST['contentOrder'];
 
 
-    $addLessonContent = $dbh->prepare('INSERT INTO lessonContent (lesson_ID, contentType, content,contentOrder ) VALUES (?, ?, ?, ?)');
+    $addLessonContent = $dbh->prepare('INSERT INTO lessonContent (lesson_ID, contentType, content,content_order ) VALUES (?, ?, ?, ?)');
     $addLessonContent->execute(array($lessonID, $contentType, $content, $contentOrder));
 
     $lessonContentData = array("contentType" => $contentType, "content" => $content);
