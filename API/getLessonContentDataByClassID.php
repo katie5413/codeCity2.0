81402f27d7f47a87994f87fd741b4c845b656afe
13 <katie87827@gmail.com>
2022-09-13 01:17:30 +0800

    fix: Remove uploadImage and textArea.

--- API/getLessonContentDataByClassID.php
+++ API/getLessonContentDataByClassID.php
@@ -7,7 +7,7 @@ $res = array();
 if (isset($_POST['classID'])) {
     // getLessonContent
     $findLessonContent = $dbh->prepare('SELECT id, contentType, content FROM lessonContent 
-    WHERE id in (SELECT DISTINCT homeworkSubmit.homework_ID
+    WHERE contentType != ? AND contentType != ? AND id in (SELECT DISTINCT homeworkSubmit.homework_ID
     FROM
         `homeworkSubmit`
     WHERE student_ID in (SELECT classEnroll.user_ID
@@ -16,7 +16,7 @@ if (isset($_POST['classID'])) {
     ON user.id = classEnroll.user_ID
     WHERE `class_ID` = ? AND `identity` != 1))
     ');
-    $findLessonContent->execute(array($_POST['classID']));
+    $findLessonContent->execute(array('uploadImage','textArea',$_POST['classID']));
 
     $lessonContentArr = array();
     while ($lessonContentItem = $findLessonContent->fetch(PDO::FETCH_ASSOC)) {
