9829885975aedb63d1be9383f937b40ad7372e5d
13 <katie87827@gmail.com>
2022-10-12 12:53:34 +0800

    fix: Remove MagicBox.

--- View/Map/index.js
+++ View/Map/index.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
                         getStudentData({ classID });
                     });
 
-                    activeMagicBox();
+                    // activeMagicBox();
                 }
 
                 activeSideMenu({
